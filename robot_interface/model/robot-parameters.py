7ed7c342b6cdf7214a6eaed25e2eb35e9dd14bab
gus <gustavosnik95@gmail.com>
2022-12-26 19:05:49 -0300

    robot parameters 0.2 class Parameters

--- robot_interface/model/robot-parameters.py
+++ robot_interface/model/robot-parameters.py
@@ -1,56 +1,50 @@
 import os
 
-import csv
-
-# Initialize an empty dictionary
-my_dict = {}
-src_path = r"D:\Arquivos HD\Projetos HD\SD Labs\JOBS\Ahmd\rocket\rocket_kitchens\Dashboard\View\Pages\output"
-filename = 'File (5).csv'
-
-filepath = os.path.join(src_path, filename)
-print(filepath)
 import pandas as pd
 
-# Read the CSV file using pandas, specifying that the first line should be skipped
-df = pd.read_csv(filepath, header=0)
+import pandas as pd
 
-# Convert the dataframe to a dictionary
-my_dict = df.to_dict()
+class Parameters:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        # Read the CSV file using pandas, specifying that the first line should be skipped
+        self.df = pd.read_csv(self.filepath, header=0)
 
-# Print the dictionary to verify that it has been created correctly
-# print(my_dict)
+    def read_csv(self):
+        # Convert the dataframe to a dictionary
+        my_dict = self.df.to_dict()
+
+        return my_dict
+
+    def get_row(self, row_index):
+        # Get the values of the specified row
+        row_values = self.df.iloc[row_index].values
+
+        # Assign the first cell value as the name of a variable
+        variable_name = row_values[0]
+
+        # Assign the rest of the cells as the values in a list
+        values = row_values[1:]
+
+        return variable_name, values
 
 
-# Convert the dictionary to a DataFrame
-df = pd.DataFrame.from_dict(my_dict)
+if __name__ == "__main__":
+    src_path = r"D:\Arquivos HD\Projetos HD\SD Labs\JOBS\Ahmd\rocket\rocket_kitchens\Dashboard\View\Pages\output"
+    filename = 'File (5).csv'
+    filepath = os.path.join(src_path, filename)
+    # Create an instance of the Parameters class
+    params = Parameters(filepath)
 
-# Print the DataFrame to verify that it has been created correctly
-# print(df.plot())
-# import seaborn as sns
-#
-# sns.lineplot(data=df)
-import matplotlib.pyplot as plt
+    # Read the CSV file and create the dictionary
+    my_dict = params.read_csv()
 
-# Create a line plot of the data
-df.plot()
+    # Print the dictionary to verify that it has been created correctly
+    # print(my_dict)
 
-# Show the plot
-plt.show()
+    # Call the get_row function, specifying the row index
+    variable_name, values = params.get_row(1)
 
-# import pandas_profiling
-#
-# profile = pandas_profiling.ProfileReport(df)
-# profile.to_html()
-# Open the CSV file and use the csv.reader function to read it
-# with open(filepath, 'r') as f:
-#     reader = csv.reader(f)
-#
-#     # Iterate through the rows of the file
-#     for row in reader:
-#         # Save the first cell as the key and the rest of the cells as the values in a list
-#         key = row[0]
-#         values = row[1:]
-#         my_dict[key] = values
-
-# Print the dictionary to verify that it has been created correctly
-print(my_dict)
+    # Print the variable name and values to verify that they have been assigned correctly
+    print(f'Variable name: {variable_name}')
+    print(f'Values: {values}')
