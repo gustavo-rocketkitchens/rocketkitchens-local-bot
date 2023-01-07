11d19e94f246a0d01f82af0710c30b02270d6393
gus <gustavosnik95@gmail.com>
2023-01-07 16:32:26 -0300

    LocalBot V1_2 ADDED Robot Interface

--- robot_interface/controller/observer_controller.py
+++ robot_interface/controller/observer_controller.py
@@ -6,27 +6,26 @@ from rocket_kitchens_local_bot.robot_interface.model.observer_model import Handl
 
 observer_process = None
 def start_controller(instance):
-    global observer_process
 
-    src_path = r"D:\Arquivos HD\Projetos HD\SD Labs\JOBS\Ahmd\rocket\rocket_kitchens\Dashboard\View\Pages\output"
+    global observer_process
 
     # Get the current working directory
     cwd = os.getcwd()
+    print("I am here: ")
     print(cwd)
-    # # Navigate to the parent directory
-    # os.chdir('..')
-    # print(cwd)
 
     # launcher_oberserver = observer_model.start_observer(src_path)
-    observer_process = subprocess.Popen(['python', 'model/observer_model.py'])
+
+    dist_robot_interface = "dependencies/observer_model.py"
+
+    # RUN WITH SCRIPT
+    # observer_process = subprocess.Popen(['python', 'model/observer_model.py'])
+
+    # RUN WITH PYSINTALLER
+    observer_process = subprocess.Popen(['python', dist_robot_interface])
 
     # Run the Python script
     print('observer_process = ', observer_process)
-
-    # Navigate back to the original working directory
-    # os.chdir(cwd)
-    # print(cwd)
-
 
 def stop_controller(instance):
     global observer_process
@@ -37,13 +36,7 @@ def stop_controller(instance):
 
     # Navigate to the parent directory
     os.chdir('..')
-    # observer_model.stop_observer()
-    # launcher_oberserver = observer_model.start_observer(src_path)
-    # launcher_oberserver = subprocess.Popen(['python', 'model/observer_model.py'])
-    # Call the stop_observer function in the observer_model script
-    # subprocess.Popen(['python', '-c', 'from model.observer_model import stop_observer; stop_observer()'])
-    # Run the Python script
-    # Stop the observer process
+
     observer_process.kill()
     print("stopping observer")
 
@@ -52,30 +45,6 @@ def stop_controller(instance):
 
 
 def start_bot_controller(instance):
-    src_path = r''
     bot_process = subprocess.Popen(['python', 'model/robot-launcher.py'])
     print(bot_process)
 
-
-
-import kivy
-import subprocess
-
-from kivy.app import App
-from kivy.uix.button import Button
-
-class MyApp(App):
-    def build(self):
-        button = Button(text="Run other script")
-        button.bind(on_press=self.on_button_press)
-        return button
-
-    def on_button_press(self, instance):
-        print("Starting Observer")
-        subprocess.Popen(['python', '../model/observer_model.py'])
-
-
-
-
-if __name__ == "__main__":
-    MyApp().run()
