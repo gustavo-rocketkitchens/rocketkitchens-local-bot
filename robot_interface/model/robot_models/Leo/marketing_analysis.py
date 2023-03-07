f68e2554b387776297502728aa204ef779e2fa25
Gustavo <gustavo@rocketkitchens.com>
2023-03-07 10:15:16 -0300

    Talabat Search: MarketingAnalysis 0.0.1.1

--- robot_interface/model/robot_models/Leo/marketing_analysis.py
+++ robot_interface/model/robot_models/Leo/marketing_analysis.py
@@ -30,7 +30,12 @@ from request_app import GetMenuItem
 
 
 # =======================================================================================================================
-
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+file_handler = logging.FileHandler('app.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 class Start:
 
